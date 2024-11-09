83a2a2e4a68070b30a8e92607d86d7acce23616d
Youness <54296289+Youness-ben@users.noreply.github.com>
2024-11-09 11:00:34 +0100

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
-export const STORAGE_URL= "http://192.168.11.9:8000/storage";
-export const MAIN_URL= "http://192.168.11.9:8000/api";
+export const STORAGE_URL= "https://api.fauconbleu.icamob.ma/storage";
+export const MAIN_URL= "https://api.fauconbleu.icamob.ma/api";
 export const MAPBOX_TOKEN="pk.eyJ1IjoidW5lcyIsImEiOiJjbG9yaTV2ZWEwdXZrMmtwZXpzdXp3dzlhIn0.imF08_9HcyjSAgFaeY6A6w";
